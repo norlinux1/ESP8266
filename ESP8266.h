8efe4c01233f36f0c52247983a7aaf7dbe92acb0
Norlinux <norlinux@sapo.pt>
2018-02-19 13:09:20 +0000

    Add files via upload

---
+++ ESP8266.h
@@ -0,0 +1,142 @@
+/*
+ * ESP8266.h
+ *
+ * Created: 02-01-2018
+ * Author : Norlinux
+ * Based in the source code Piotr Styczy?ski
+ * http://www.microelectronic.pt
+ * http://maquina.96.lt
+ * https://www.facebook.com/MundoDosMicrocontroladores/
+ * Released under GPLv3.
+ * Please refer to LICENSE file for licensing information.
+ * which can be found at http://www.gnu.org/licenses/gpl.txt
+ */ 
+
+
+#ifndef ESP8266_H_
+#define ESP8266_H_
+
+/**
+ * @def Buffer_Len
+ * Determines buffer length of internal command buffer.
+ *
+ * The responses from ESP8266 normally should never fill this up!
+ */
+#define Buffer_Len 700
+
+
+/**
+ * @def WAIT_FOR_DATA
+ *
+ * Value signalizator for WIFI_Listen to listen for incoming data.
+ * Listening would end if and only if:
+ *   * status OK/ERROR is returned from ESP8266
+ *   * timeout event occurs when waiting for data
+ *   * ESP8266 received data and returns +IPD command
+ *     then the data is captured and listening ends.
+ */
+#define WAIT_FOR_DATA 1
+#define ESP_Mode_STA  1                                /*!< ESP in station mode */
+#define ESP_Mode_AP  2                                 /*!< ESP as software Access Point mode */
+#define ESP_Mode_STA_AP  3							   /*!< ESP station mode Both*/
+#define Single  0									   /*!< ESP Single Connection*/
+#define Multiple  1									   /*!< ESP Multiple Connection*/
+
+/******************************************************************************/
+/***                           Private structures                           ***/
+/******************************************************************************/
+
+typedef struct {
+	uint8_t Length;
+	uint8_t Data[128];
+} Received_t;
+/**
+ * \brief   Low level structure for driver
+ * \note    For now it has basic settings only without hardware flow control.
+ */
+typedef struct _ESP_Baud_t {
+    uint32_t Baudrate;          /*!< Baudrate to be used for UART */
+} ESP_Baud_t;
+
+/**
+ * \brief           ESP8266 library possible return statements on function calls
+ */
+typedef enum _ESP_Result {
+	espERROR = 0,                                    /*!< An error occurred */
+	espOK,											    /*!< Everything is OK */
+	espDEVICENOTCONNECTED,                              /*!< Device is not connected to UART or wrong USSID */
+	espTIMEOUT,                                         /*!< Timeout was detected when sending command to ESP module */
+	espTIMEandPASS,										/*!< Timeout or Wrong password detected when sending command to ESP module */
+	espWIFINOTCONNECTED,                                /*!< Wifi is not connected to network */
+	espBUSY,                                            /*!< Device is busy, new command is not possible */
+	espINVALIDPARAMETERS,                               /*!< Parameters for functions are invalid */
+    espSENDERROR,                                       /*!< Error trying to send data on connection */
+    espNOTFOUNDTARGETAP,                                /*!< Fail on connect from ESP-STA to ESP-AP */ 
+    espWRONGPASSWORD,                                    /*!< Password is wrong */
+	espRESTART											/*!< ESP8266 RESTART */
+} ESP_Result;
+
+
+typedef enum _ESP_Control_t {
+    /**
+     * \brief       Called to initialize low-level part of device, such as UART and GPIO configuration
+     *
+     * \param[in]   *param: Pointer to \ref ESP_LL_t structure with baudrate setup
+     * \param[out]  *result: Pointer to \ref uint8_t variable with result. Set to 0 when OK, or non-zero on ERROR.
+     */
+    ESP_Control_Init = 0x00,     /*!< Initialization control */
+    
+    /**
+     * \brief       Called to send data to ESP device
+     *
+     * \param[in]   *param: Pointer to \ref ESP_LL_Send_t structure with data to send
+     * \param[out]  *result: Pointer to \ref uint8_t variable with result. Set to 0 when OK, or non-zero on ERROR.
+     */
+    ESP_Control_Send,            /*!< Send data control */   
+} ESP_Control_t;
+
+/**
+ * \brief   Structure for sending data to low-level part
+ */
+typedef struct _ESP_Send_t {
+    const uint8_t* Data;            /*!< Pointer to data to send */
+    uint16_t Count;                 /*!< Number of bytes to send */
+    uint8_t Result;                 /*!< Result of last send */
+} ESP_Send_t;
+
+extern ESP_Result   ESP_Begin(uint32_t);
+extern uint8_t		ESP_Callback(ESP_Control_t , void* , void* );
+extern ESP_Result   WIFI_Listening(char*, char*,char*);
+extern ESP_Result	ESP_Init();
+extern ESP_Result	ESP_Restart(void);
+extern ESP_Result	ESP_WIFIMode(uint8_t , uint8_t );
+extern ESP_Result	ESP_WIFIConnect(const char* , const char*);
+extern ESP_Result   WIFI_Client_Connection (const char*, const char*, const int);
+extern ESP_Result	WIFI_Send (const char* );
+static ESP_Send_t Send;                                         /* Send data setup */
+
+#define FROMMEM(x)                          ((const char *)(x))
+#define UART_SEND_STR(str)                  do { Send.Data = (const uint8_t *)(str); Send.Count = strlen((const char *)(str)); ESP_Callback(ESP_Control_Send, &Send, &Send.Result); } while (0)
+#define UART_SEND(str, len)                 do { Send.Data = (const uint8_t *)(str); Send.Count = (len); ESP_Callback(ESP_Control_Send, &Send, &Send.Result); } while (0)
+#define UART_SEND_CH(ch)                    do { Send.Data = (const uint8_t *)(ch); Send.Count = 1; ESP_Callback(ESP_Control_Send, &Send, &Send.Result); } while (0)
+
+#define RESP_OK                             ("\rOK\r")
+#define RESP_OK_CR							("OK\r")
+#define RESP_ATE_OK							("ATE0\r\rOK\r")
+#define RESP_ERROR                          ("\rERROR\r")
+#define RESP_BUSY                           ("\rbusy p...\r")
+#define RESP_READY                          ("\rready\r")
+#define RESP_GOT_IP							("WIFI GOT IP\r")
+#define RESP_CONNECT						("WIFI CONNECTED\r")
+#define RESP_CONNECTED						("CONNECT\r")
+#define RESP_CONNECT_CR						("\rCONNECT\r")
+#define RESP_CONNECT_OK						("CONNECT\r\rOK\r")
+#define RESP_DISCONNECT						("WIFI DISCONNECT\r")
+#define RESP_ATMEGA							("atmega.php\r")
+#define RESP_CLOSED							("CLOSED\r")
+#define RESP_SEND_OK						("SEND OK\r")
+#define _CRLF                               FROMMEM("\r\n")
+
+#define ESP8266_listen(str,alt1,alt2)		WIFI_Listening(str,alt1,alt2)			
+
+#endif /* ESP8266_H_ */
---
+++ USART_V1.h
@@ -0,0 +1,99 @@
+/*
+* USART.h
+* Created: 22-12-2017 
+* Copyright by Peter Fleury (C)
+* Adapted By Author : Norlinux
+*http://www.microelectronic.pt
+*http://maquina.96.lt
+*https://www.facebook.com/MundoDosMicrocontroladores/
+*Released under GPLv3.
+*Please refer to LICENSE file for licensing information.
+*which can be found at http://www.gnu.org/licenses/gpl.txt
+*/
+
+
+#ifndef USART_H_
+#define USART_H_
+#define UART_BAUD_SELECT(baudRate,xtalCpu) (((xtalCpu)+8UL*(baudRate))/(16UL*(baudRate))-1UL)
+/***************************************************************************************************
+                     Macros to extract the Byte
+***************************************************************************************************/
+#define util_ExtractByte0to8(x)    (uint8_t) ((x) & 0xFFu)
+#define util_ExtractByte8to16(x)   (uint8_t) (((x)>>8) & 0xFFu)
+// baude for 16Mhz
+#define M_GetBaudRateGeneratorValue(baudrate)  (((16000000UL -((baudrate) * 8L)) / ((baudrate) * 16UL)))
+/***************************************************************************************************
+                             Baudrate configurations
+***************************************************************************************************/
+#define C_MinBaudRate_U32 2400
+#define C_MaxBaudRate_U32 115200UL
+
+/**************************************************************************************************/
+
+/*
+** high byte error return code of uart_getc()
+*/
+#define UART_FRAME_ERROR      0x0800              /**< Framing Error by UART       */
+#define UART_OVERRUN_ERROR    0x0400              /**< Overrun condition by UART   */
+#define UART_BUFFER_OVERFLOW  0x0200              /**< receive ringbuffer overflow */
+#define UART_NO_DATA          0x0100              /**< no receive data available   */
+/* Set size of receive and transmit buffers */
+
+
+#define UART_RX0_BUFFER_SIZE 256 /**< 128 Size of the circular receive buffer, must be power of 2 */
+#define UART_TX0_BUFFER_SIZE 256 /**< 128 Size of the circular transmit buffer, must be power of 2 */
+
+
+/* size of RX/TX buffers */
+#define UART_RX0_BUFFER_MASK (UART_RX0_BUFFER_SIZE - 1)
+#define UART_TX0_BUFFER_MASK (UART_TX0_BUFFER_SIZE - 1)
+static volatile uint8_t UART_TxBuf[UART_TX0_BUFFER_SIZE];
+static volatile uint8_t UART_RxBuf[UART_RX0_BUFFER_SIZE];
+static volatile uint8_t UART_TxHead;
+static volatile uint8_t UART_TxTail;
+static volatile uint8_t UART_RxHead;
+static volatile uint8_t UART_RxTail;
+static volatile uint8_t UART_LastRxError;
+/* Macros, to allow use of legacy names */
+
+/** @brief Macro to initialize USART0 (only available on selected ATmegas) @see uart0_init */
+#define uart_init(b)      uart0_init(b)
+
+/** @brief Macro to get received byte of USART0 from ringbuffer. (only available on selected ATmega) @see uart0_getc */
+#define uart_getc()       uart0_getc()
+
+///** @brief Macro to peek at next byte in USART0 ringbuffer */
+//#define uart_peek()       uart0_peek()
+
+/** @brief Macro to put byte to ringbuffer for transmitting via USART0 (only available on selected ATmega) @see uart0_putc */
+#define uart_putc(d)      uart0_putc(d)
+
+/** @brief Macro to put string to ringbuffer for transmitting via USART0 (only available on selected ATmega) @see uart0_puts */
+#define uart_puts(s)      uart0_puts(s)
+
+/** @brief Macro to put string from program memory to ringbuffer for transmitting via USART0 (only available on selected ATmega) @see uart0_puts_p */
+#define uart_puts_p(s)    uart0_puts_p(s)
+
+/** @brief Macro to return number of bytes waiting in the receive buffer of USART0 @see uart0_available */
+#define uart_available()  uart0_available()
+
+/** @brief Macro to flush bytes waiting in receive buffer of USART0 @see uart0_flush */
+#define uart_flush()      uart0_flush()
+
+#define Baud_9600   103
+#define Baud_115200 8
+
+extern void		USART_Init(uint32_t);
+extern void		USART_Init_baud(uint16_t);
+extern void		uart0_init(uint16_t);
+extern uint16_t uart0_getc();
+extern void		uart0_putc(uint8_t );
+extern void		uart0_puts(const char *);
+extern void		uart0_puts_p(const char *);
+extern uint16_t uart0_available();
+extern void		uart0_flush();
+// declaration functions
+extern void		putUsart0(unsigned char );
+// declarations for ESP8266
+extern void		ESP8266_Send(uint8_t* , uint16_t );
+#endif /* USART_H_ */
